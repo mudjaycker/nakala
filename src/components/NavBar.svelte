3f4f0b764be555b17d3d7f09ce4e46fb8df52f49
mudjaycker <tombutoyi@gmail.com>
2023-09-22 03:27:07 +0200

    update navebar

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -18,20 +18,5 @@
     display: flex;
     justify-content: space-between;
   }
-  button{
-    all: unset;
-    background-color: rgb(255, 208, 0);
-    color: var(--color3);
-    padding: 1%;
-    font-size: 0.8em;
-    text-align: center;
-    width: 35%;
-    font-weight: bold;
- }
-  button:hover{
-    background-color: var(--color3);
-    color: white;
-  }
-
 
 </style>
