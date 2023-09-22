6edc2070b8d3ff9ec70b127c8d6a4a14cf3e6125
mudjaycker <tombutoyi@gmail.com>
2023-09-22 03:26:46 +0200

    init remender field

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,10 @@
   import { onMount } from "svelte";
   //@ts-nocheck
   import Navbar from "./components/NavBar.svelte";
+  import ReminderField from "./components/ReminderField.svelte";
 </script>
 
 <main>
   <Navbar />
+  <ReminderField />
 </main>
---
+++ src/components/ReminderField.svelte
@@ -0,0 +1,33 @@
+<script>
+  let reminder = {
+    title: "",
+    text: "",
+  };
+  $: save = function () {
+    let string = JSON.stringify(reminder);
+    localStorage.setItem("reminder", string);
+  };
+</script>
+
+<section>
+  <div>
+    <input type="text" placeholder="Title" bind:value={reminder.title} />
+    <textarea name="" id="" cols="28" rows="8" bind:value={reminder.text} />
+    <button type="button" on:click={save}>add</button>
+  </div>
+</section>
+
+<style>
+  section {
+    margin-top: 20%;
+    margin-left: 10%;
+    padding: 10%;
+  }
+  textarea {
+    width: 90%;
+    margin-top: 2%;
+  }
+  input {
+    width: 90%;
+  }
+</style>
