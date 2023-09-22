088d3cb87febe6fbd69f42e1a19aa0789a7f7cc8
mudjaycker <tombutoyi@gmail.com>
2023-09-22 02:43:22 +0200

    init buttons

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  import {onMount} from "svelte"
+  import { onMount } from "svelte";
   //@ts-nocheck
-  import Title from "./components/Title.svelte";
+  import Navbar from "./components/NavBar.svelte";
 </script>
 
 <main>
-  <Title></Title>
+  <Navbar />
 </main>
---
+++ src/components/NavBar.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  let count: number = 0;
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<section>
+  <nav>
+    <button> Add reminder </button>
+    <button> Reminder list </button>
+  </nav>
+</section>
+
+<style>
+  nav {
+    background: var(--color1);
+    display: flex;
+    justify-content: space-between;
+  }
+  button{
+    all: unset;
+    background-color: rgb(255, 208, 0);
+    color: var(--color3);
+    padding: 1%;
+    font-size: 0.8em;
+    text-align: center;
+    width: 35%;
+    font-weight: bold;
+ }
+  button:hover{
+    background-color: var(--color3);
+    color: white;
+  }
+
+
+</style>
