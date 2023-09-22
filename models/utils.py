8667a9d532ccc2bff3d34bef3a57e72986a0908a
mudjaycker <tombutoyi@gmail.com>
2023-09-22 13:48:21 +0200

    update async_to_sync so that we get the value returned from it

--- models/utils.py
+++ models/utils.py
@@ -4,8 +4,9 @@ from typing import Callable
 def async_to_sync(func: Callable):
     def inner(*args, **kwargs):
         async def create_task(*args, **kwargs):
-            await asyncio.create_task(func(*args, **kwargs))
+            return await asyncio.create_task(func(*args, **kwargs))
 
-        asyncio.run(create_task(*args, **kwargs))
+        return asyncio.run(create_task(*args, **kwargs))
+        
 
     return inner
