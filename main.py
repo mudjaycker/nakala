d397a2e7dfc134ee6dbc5ddd5f1373372a5f8a83
mudjaycker <tombutoyi@gmail.com>
2023-09-22 02:05:11 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import webview
+from tkinter import Tk
+
+root = Tk()
+
+sx= root.winfo_screenwidth()
+sy = root.winfo_screenheight()
+
+
+# percent = lambda x, p: (p*x)/100 
+
+webview.create_window(
+    "nakala",
+    "http://localhost:5173/",
+    background_color="#FF0000",
+    width=300,
+    height=500,
+    on_top=True,
+    x=sx-310,
+    y=sy-550
+)
+webview.start()
---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import {onMount} from "svelte"
+  //@ts-nocheck
+  import Title from "./components/Title.svelte";
+</script>
+
+<main>
+  <Title></Title>
+</main>
