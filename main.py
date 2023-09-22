d8dfdca7adf26de104924d752956fd12b6303503
mudjaycker <tombutoyi@gmail.com>
2023-09-22 11:55:58 +0200

    create a async_to_sync decorator to use with tortoise async functions further so that front get them

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import webview
 from tkinter import Tk
+from utils import async_to_sync
 
 root = Tk()
 
-sx= root.winfo_screenwidth()
+sx = root.winfo_screenwidth()
 sy = root.winfo_screenheight()
 
 
-# percent = lambda x, p: (p*x)/100 
 
+class Api:
+    @async_to_sync
+    async def hello(self, name):
+        print("===> hello", name)
+
+
+# percent = lambda x, p: (p*x)/100
+dev_url = "http://localhost:5173/"
+test_url = "dist/index.html"
+api = Api()
 webview.create_window(
     "nakala",
-    "dist/index.html",
+    dev_url,
     background_color="#4f384a",
     width=300,
     height=500,
     on_top=True,
-    x=sx-310,
-    y=sy-550
+    x=sx - 310,
+    y=sy - 550,
+    js_api=api,
 )
-webview.start()
+webview.start(debug=True)
---
+++ utils.py
@@ -0,0 +1,11 @@
+import asyncio
+from typing import Callable
+
+def async_to_sync(func: Callable):
+    def inner(*args, **kwargs):
+        async def create_task(*args, **kwargs):
+            await asyncio.create_task(func(*args, **kwargs))
+
+        asyncio.run(create_task(*args, **kwargs))
+
+    return inner
