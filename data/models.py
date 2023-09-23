9b4e65a10c9d557d7245eea36f1b215421e6bce7
mudjaycker <tombutoyi@gmail.com>
2023-09-23 11:54:16 +0200

    add datetime field

--- data/models.py
+++ data/models.py
@@ -3,5 +3,6 @@ from tortoise import fields
 
 class Reminder(Model):
     id = fields.IntField(pk=True)
-    title = fields.CharField(max_length=26)
-    text = fields.TextField()
+    title = fields.CharField(max_length=26, null=False, blank=False)
+    text = fields.TextField(null=False, blank=False)
+    created_at = fields.DatetimeField(auto_now_add=True)
