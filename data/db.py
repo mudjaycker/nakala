508beec8bcdf63ef0ccd15b468908ae591d747ba
mudjaycker <tombutoyi@gmail.com>
2023-09-27 21:44:02 +0200

    importance level combobox

--- data/db.py
+++ data/db.py
@@ -2,6 +2,7 @@ from .init_db import *
 from .utils import async_to_sync
 from .models import Reminder
 
+
 class API:
     @async_to_sync
     async def create(self, reminder):
@@ -10,5 +11,19 @@ class API:
 
     @async_to_sync
     async def get(self):
-        datas = [{"title": reminder.title, "text": reminder.text} async for reminder in Reminder.all()]
+        datas = [
+            {
+                "title": reminder.title,
+                "text": reminder.text,
+                "importance": reminder.importance,
+                "date": reminder.created_at,
+            }
+            async for reminder in Reminder.all()
+        ]
         return datas
+
+
+# api = API()
+# r = Reminder(title="test", text="test", importance=1)
+# print(r)
+# print(api.get())
--- data/models.py
+++ data/models.py
@@ -2,14 +2,19 @@ from typing import Any
 from tortoise.models import Model
 from tortoise import fields
 
+
 class Reminder(Model):
     id = fields.IntField(pk=True)
     title = fields.CharField(max_length=26, null=False, blank=False)
     text = fields.TextField(null=False, blank=False)
-    importance = fields.CharField(max_length=26, null=False, blank=False, default="Hi")
+    importance = fields.IntField(max_length=26, null=False, blank=False, default=0)
     created_at = fields.DatetimeField(auto_now_add=True)
 
     def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
-        # constraint importance must be one of ("Hi", "Normal")
-        assert self.importance in ("Hi", "Normal", "Low")
+        # constraint importance must be one of ("Hi", "Normal", "Low")
+        self.importances = ["Low", "Normal", "High"]
+        assert self.importance in [self.importances.index(i) for i in self.importances]
+
+    def __str__(self) -> str:
+        return f"{self.title=} {self.importances[self.importance]=} {self.created_at=}"
