d08ea18d50034e5370b7e46c099ca96b98259459
mudjaycker <tombutoyi@gmail.com>
2025-01-19 20:32:29 +0200

    debug from backend

--- main.py
+++ main.py
@@ -8,19 +8,18 @@ sx = root.winfo_screenwidth()
 sy = root.winfo_screenheight()
 
 
-# percent = lambda x, p: (p*x)/100
-dev_url = "http://localhost:5173/"
-production_url = "dist/index.html"
+url = "http://localhost:5173/"
+# url = "dist/index.html"
 api = API()
 webview.create_window(
     "nakala",
-    production_url,
+    url,
     background_color="#4f384a",
-    width=325,
-    height=510,
+    width=sx // 6,
+    height=sy // 2,
     on_top=True,
-    x=sx - 310,
-    y=sy - 550,
+    x=sx,
+    y=sy,
     js_api=api,
 )
-webview.start(debug=False, gui="qt")
+webview.start(gui="qt", debug=True)
